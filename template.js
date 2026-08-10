e5f6fb26a05b5d0e4c6787ec174f5c0093757eaf
krambo345 <krambo345@gmail.com>
2026-08-10 17:59:39 -0600

    upd @

--- template.js
+++ template.js
@@ -1,2 +1,11 @@
 const kernel = window.modOS.kernel;
 const display = document.querySelector(".display");
+export async function app(){
+
+}
+export async function kill(){
+
+}
+export async function commands(){
+    
+}
