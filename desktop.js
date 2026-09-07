707f47982ecb99f3e13ce9f2635368579fcabd5e
Huseyin <krambo345@gmail.com>
2026-09-07 13:20:41 -0500

    Change package type check from 'cli' to 'gui'

--- desktop.js
+++ desktop.js
@@ -74,7 +74,7 @@ export async function app() {
 
     installedIds.forEach((id) => {
       const packageData = Array.isArray(pckgs) ? pckgs.find((p) => p.id === id) : null;
-      if (!packageData || !packageData.type == "cli") return;
+      if (!packageData || packageData.type == "gui") return;
       fragment.appendChild(buildIcon(packageData));
     });
 
